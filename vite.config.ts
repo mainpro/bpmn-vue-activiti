d3267e82f8d1c7ae47e217ad897bd8eae3a1ade4
dipu <mainpro@126.com>
2021-05-13 15:20:18 +0800

    初始化

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+const path = require('path')
+
+//直接获取文件的text
+function rawTransform(
+  fileRegex: Array<RegExp>,
+): {
+  name: string;
+  transform: (src: string, id: string) => string | void;
+} {
+  return {
+    name: 'get-file-raw',
+    transform(src, id): string | void {
+      if (fileRegex.filter((re) => re.test(id)).length > 0) {
+        return `export default ${JSON.stringify(src)}`;
+      }
+    },
+  };
+}
+export default {
+  alias: {
+    // 键必须以斜线开始和结束
+    '@': path.resolve(__dirname, './src')
+  },
+  base: './',
+  port: 3000,
+  optimizeDeps: {
+    //声明深度路径模块
+    include: [
+      'bpmn-js/lib/Modeler',
+      'highlight.js',
+      'codemirror',
+      'codemirror/mode/xml/xml.js',
+      'codemirror/addon/hint/xml-hint.js',
+      'bpmn-js/lib/features/label-editing/LabelUtil.js',
+    ],
+  },
+  plugins: [vue(), rawTransform([/\.bpmn$/]), vueJsx()],
+};
